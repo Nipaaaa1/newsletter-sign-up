cd484fe2da6c539a5cd75c41d2b43639a5a325ee
Nipaaaa1 <Frostone224@gmail.com>
2024-02-01 15:28:15 +0700

    Mobile Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
+import mobileIllustration from "/images/illustration-sign-up-mobile.svg";
+
+import successIcon from "/images/icon-success.svg";
+
 const App = () => {
-  return ( <h1>Hello World</h1> );
-}
- 
+  return (
+    <main>
+      <img
+        className="h-max w-full"
+        src={mobileIllustration}
+        alt="Mobile Illustration"
+      />
+      <section className="font-roboto flex flex-col gap-5 px-5 pb-6 pt-10 text-darkSlateGrey antialiased">
+        <h1 className="text-[2.5rem] font-bold">Stay updated!</h1>
+        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+        <ul className="space-y-3">
+          <li className="flex gap-4">
+            <img className="h-6 w-max" src={successIcon} alt="Success Icon" />
+            Product discovery and building what matters
+          </li>
+          <li className="flex gap-4">
+            <img className="h-6 w-max" src={successIcon} alt="Success Icon" />
+            Measuring to ensure updates are a success
+          </li>
+          <li className="flex gap-4">
+            <img className="h-6 w-max" src={successIcon} alt="Success Icon" />
+            And much more!
+          </li>
+        </ul>
+      </section>
+      <form action="/" className="flex flex-col p-5">
+        <label
+          className="mb-3 text-sm font-bold text-darkSlateGrey"
+          htmlFor="email"
+        >
+          Email address
+        </label>
+        <input
+          className="w-full rounded-lg border-2 p-5"
+          name="email"
+          placeholder="email@company.com"
+          type="email"
+        />
+        <button
+          className="font-roboto mt-7 w-full rounded-lg bg-darkSlateGrey py-5 text-sm font-bold text-white"
+          type="submit"
+        >
+          Subscribe to monthly newsletter
+        </button>
+      </form>
+    </main>
+  );
+};
+
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,10 @@ export default {
         tomato: "hsl(4, 100%, 67%)",
         darkSlateGrey: "hsl(234, 29%, 20%)",
         charcoalGrey: "hsl(235, 18%, 26%)",
-        grey: "Grey: hsl(231, 7%, 60%)",
+        grey: "hsl(231, 7%, 60%)",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
       },
     },
   },
